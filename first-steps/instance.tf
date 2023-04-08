8fedadbc82ce3545c6291fc118eaa3aad21597c9
sophware <sophware@hotmail.com>
2023-04-07 22:46:00 -0400

    my way

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
+  # access_key = "ACCESS_KEY_HERE"
+  # secret_key = "SECRET_KEY_HERE"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-0fec2c2e2017f4e7b"
   instance_type = "t3.micro"
+  key_name = "aws_ed25519"
 }
 
